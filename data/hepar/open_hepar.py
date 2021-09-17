19e1c906647ce1aaf079769696f10547ad3f6be6
ChristophLuther <cph.luther@gmail.com>
2021-09-17 16:26:05 +0200

    files to load dfs

---
+++ data/hepar/open_hepar.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df1 = pd.read_csv(f"data/hepar/hepar_s.csv")
+df2 = pd.read_csv(f"data/hepar/hepar_m.csv")
+df3 = pd.read_csv(f"data/hepar/hepar_l.csv")
+df4 = pd.read_csv(f"data/hepar/hepar_xl.csv")
+df5 = pd.read_csv(f"data/hepar/hepar_train.csv")
+df6 = pd.read_csv(f"data/hepar/hepar_test.csv")
